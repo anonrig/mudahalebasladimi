298cd41cf20c84973211cf6d3876b7b473187fac
ynizipli <ynizipli@sabanciuniv.edu>
2014-03-13 02:26:04 +0200

    Try to fix CORS.

--- twitter.py
+++ twitter.py
@@ -1,11 +1,55 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, current_app  
 from flask_cors import cross_origin
 from datetime import datetime
+from datetime import timedelta
+from flask import make_response, request, current_app
+from functools import update_wrapper
 import twitter
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+    
 app = Flask(__name__)
 api = twitter.Api(
 	consumer_key= 'NfrOmg2osL5l80uNCwqQ',
@@ -14,13 +58,14 @@ api = twitter.Api(
     access_token_secret= 'Y2CpbokN04JN7PHxsGWcym036it06yogWPeliBNWw4DW8'
 )
 
-@cross_origin()
-@app.route("/")
+@crossdomain(origin='*') 
+@app.route("/", methods=['GET', 'OPTIONS'])
 def hello():
     return "Hello World!"
     
-@app.route('/api', methods=['GET'])
-@app.route('/api/search/since/<int:sinceId>', methods=['GET'])
+@crossdomain(origin='*')    
+@app.route('/api', methods=['GET', 'OPTIONS'])
+@app.route('/api/search/since/<int:sinceId>', methods=['GET', 'OPTIONS'])
 def search(sinceId=None):
 	resultSet = []
 	
